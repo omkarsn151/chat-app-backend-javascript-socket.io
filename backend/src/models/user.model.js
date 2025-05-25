220694443b741c25f9705ed7be39c5261e76ec85
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-05-25 15:18:17 +0530

    jwt tokens

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -9,6 +9,13 @@ const userSchema = new Schema(
             trim: true,
             index: true,
         },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true,
+        },
         fullName: {
             type: String,
             required: true,
@@ -42,6 +49,35 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     }
     return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+        }
+    );
+};
+
+// Method to generate a refresh token
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+        }
+    );
+};
+
 
 
 export const User = mongoose.model("User", userSchema);
